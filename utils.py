0b3711e19f711b25e55d3e5c7cf6f5e5200f7f05
Pav Sidhu <pav@pavsidhu.com>
2018-12-03 20:34:35 +0000

    Add split_email util

--- utils.py
+++ utils.py
@@ -1,1 +1,15 @@
+import re
+
 flatten = lambda list: [item for sublist in list for item in sublist]
+
+
+def split_email(email):
+    """Splits an email into headers and the abstract"""
+
+    pattern = r"([\S\s]+?Abstract: \n)([\S\s]+)"
+    split_email = re.search(pattern, email)
+
+    headers = split_email.group(1)
+    abstract = split_email.group(2)
+
+    return [headers, abstract]
